7d9e5db6e21f18523e97408c537444c38d46598b
LuisMIguelFurlanettoSousa <lfurlanettosousa@gmail.com>
2026-06-22 20:19:19 -0300

    feat: expor o status real do banco no endpoint /api/v1/status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,30 @@
 import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as rows;");
-  console.log(result.rows);
+  const updateAt = new Date().toISOString();
+
+  const postgresVersion = (await database.query("SHOW server_version;")).rows[0]
+    .server_version;
+
+  const maxConnections = Number(
+    (await database.query("SHOW max_connections;")).rows[0].max_connections,
+  );
+
+  const databaseName = process.env.POSTGRES_DB;
+  const activeConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname=$1;",
+    values: [databaseName],
+  });
+
   res.status(200).json({
-    chave: "teste de api",
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        postgres_version: postgresVersion,
+        max_connections: maxConnections,
+        active_connections: activeConnections.rows[0].count,
+      },
+    },
   });
 }
 
