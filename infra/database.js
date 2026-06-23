088c3f0a292f9387bc6bf23f63ccffe66eae57e5
LuisMIguelFurlanettoSousa <lfurlanettosousa@gmail.com>
2026-06-23 06:55:32 -0300

    logando as credencias do postgres

--- infra/database.js
+++ infra/database.js
@@ -9,6 +9,14 @@ async function query(objectQuerry) {
     password: process.env.POSTGRES_PASSWORD,
   });
 
+  console.log("Credencias do postgres:", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+
   try {
     client.connect();
     const result = await client.query(objectQuerry);
