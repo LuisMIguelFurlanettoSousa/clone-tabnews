e0cb25f159a0deb1b964f31ceb783387d265de97
LuisMIguelFurlanettoSousa <lfurlanettosousa@gmail.com>
2026-06-24 06:58:02 -0300

    removendo  denecessário

--- infra/database.js
+++ infra/database.js
@@ -10,14 +10,6 @@ async function query(objectQuerry) {
     ssl: getSslValues(),
   });
 
-  console.log("Credencias do postgres:", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  });
-
   try {
     client.connect();
     const result = await client.query(objectQuerry);
